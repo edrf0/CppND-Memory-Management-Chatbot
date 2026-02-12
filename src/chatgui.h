aeafaaaa7420c46569748d3fb77d1c1764c854ab
edrf0 <125705907+edrf0@users.noreply.github.com>
2026-02-12 17:03:13 -0400

    .

--- src/chatgui.h
+++ src/chatgui.h
@@ -1,7 +1,11 @@
 #ifndef CHATGUI_H_
 #define CHATGUI_H_
 
+#include <memory>
 #include <wx/wx.h>
+
+#include "chatlogic.h"
+#include "chatlogic.h"
 
 class ChatLogic; // forward declaration
 
@@ -16,7 +20,8 @@ private:
     //// STUDENT CODE
     ////
 
-    ChatLogic *_chatLogic;
+    //ChatLogic *_chatLogic;
+    std::unique_ptr<ChatLogic> _chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +32,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
